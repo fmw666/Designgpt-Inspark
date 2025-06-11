388b69bfb3e71278fed5c2b52ad8f74304dcf2e8
fmw666 <fmw19990718@gmail.com>
2025-06-11 10:54:39 +0800

    update: 添加调试打印信息

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -199,11 +199,13 @@ export const ChatMessage: FC<ChatMessageProps> = ({
                           key={index}
                           data-result-id={result.id}
                           className="group relative aspect-square min-w-[200px] rounded-lg overflow-hidden border border-gray-200 dark:border-gray-800 bg-transparent cursor-pointer bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px] dark:bg-[linear-gradient(to_right,#ffffff12_1px,transparent_1px),linear-gradient(to_bottom,#ffffff12_1px,transparent_1px)]"
-                          onClick={() => setSelectedImage({
+                          onClick={() => {setSelectedImage({
                             url: result.url,
                             messageId: message.id,
                             resultId: result.id,
-                          })}
+                          });
+                          console.log('click');
+                        }}
                         >
                           {result.isGenerating ? (
                             isTimedOut ? (
